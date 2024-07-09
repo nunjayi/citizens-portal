9c81eaf80f6642d62db4b4ed507ec2d62d80f0c4
nunjayi <epenyash@gmail.com>
2024-07-09 11:16:13 +0300

    setting up the backend

---
+++ backend/app.py
@@ -0,0 +1,25 @@
+# server/app.py
+
+from flask import Flask
+from flask_migrate import Migrate
+
+
+
+# create a Flask application instance 
+app = Flask(__name__)
+
+# configure the database connection to the local file app.db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+# configure flag to disable modification tracking and use less memory
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# create a Migrate object to manage schema modifications
+
+
+# initialize the Flask application to use the database
+
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
---
+++ backend/models.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+
+# contains definitions of tables and associated schema constructs
+metadata = MetaData()
+
+# create the Flask SQLAlchemy extension
+db = SQLAlchemy(metadata=metadata)
+
+# define a model class by inheriting from db.Model.
